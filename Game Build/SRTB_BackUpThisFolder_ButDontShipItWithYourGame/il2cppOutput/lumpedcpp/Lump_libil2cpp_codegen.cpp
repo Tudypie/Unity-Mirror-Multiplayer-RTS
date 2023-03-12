e7102a8aef9def5ec27594191bebdae838378ca2
Tudypie <33548577+TudyPie@users.noreply.github.com>
2023-03-12 18:06:19 +0200

    Steam transport finally working

--- Game Build/SRTB_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
--- Game Build/SRTB_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,6 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\vm-utils\BlobReader.cpp"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\vm-utils\Debugger.cpp"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\vm-utils\VmStringUtils.cpp"
