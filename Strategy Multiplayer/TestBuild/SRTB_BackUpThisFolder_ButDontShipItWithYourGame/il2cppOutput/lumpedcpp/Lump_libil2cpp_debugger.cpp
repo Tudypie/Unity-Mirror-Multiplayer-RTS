e4024b8d703a3b3625b0702b443b7e786b83f683
Tudypie <33548577+TudyPie@users.noreply.github.com>
2023-03-12 19:42:38 +0200

    New Game Build

--- Strategy Multiplayer/TestBuild/SRTB_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\debugger\il2cpp-stubs.cpp"
--- Strategy Multiplayer/TestBuild/SRTB_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\mono-runtime\il2cpp-callbacks.cpp"
-#include "E:\Projects\Unity Projects\Versions\2019.4.18f1\Editor\Data\il2cpp\libil2cpp\mono-runtime\il2cpp-mono-support.cpp"
